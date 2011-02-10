33b68801053605bed19b6974469bf3e082e7eb80
Vlad <vlad_micutul@yahoo.com>
2011-02-10 08:26:13 +0200

    Added romanian language.

---
+++ lang/lang.ro.php
@@ -0,0 +1,35 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad_micutul@yahoo.com',
+		),
+		'release-date' => '2011-02-10'
+	);
+
+	/**
+	 * Readonly Mode
+	 */
+	$dictionary = array(
+
+		'Readonly Mode' => 
+		'Exclusiv citire',
+
+		'Enable readonly mode' => 
+		'Activează modul Exclusiv Citire',
+
+		'Radonly mode will prevent all authors and events from editing or creating entries.' => 
+		'Modul Exclusiv Citire va opri autorii şi evenimentele să editeze sau să creeze înregistrări.',
+
+		'This site is currently in readonly mode.' =>
+		'Acest site este momentan în modul Exclusiv Citire. <strong>Nu</strong> puteţi edita sau crea înregistrări.',
+		
+		'Restore?' =>
+		'Resetează?',
+		
+		'Events have been disabled temporarily.' =>
+		'Momentan evenimentele au fost dezactivate.',
+
+	);
